60a003840932eccca5dc327831d0c0d7e36c4ec7
LILPIG <lilpig@DESKTOP-40DII3I.localdomain>
2021-11-18 08:42:00 +0800

    重构Promise.all

--- enhance_method.ts
+++ enhance_method.ts
@@ -14,33 +14,24 @@ function reject(reason: any): DogePromise {
 
 function all(promises: DogePromise[]): DogePromise {
   return new DogePromise((resolve, reject) => {
-    // 将reject和resolve分离处理
-    (function checkReject(i: number) {
-      // 分离处理和这里的setTimeout都是为了保证发现按时间顺序最先rejected的Promise并直接reject
-      // 如果同步执行，那么它发现的是按下标顺序的第一个reject的而不是按reject时间的
+    let fulfilledCnt = 0;
+    let fulfilledValues = [];
+    promises.forEach((p, i) => {
       setTimeout(() => {
-        if (i < promises.length) {
-          promises[i].then(null, function onRejected(reason) {
+        p.then(
+          (value) => {
+            fulfilledValues[i] = value;
+            fulfilledCnt++;
+            if (fulfilledCnt === promises.length) {
+              resolve(fulfilledValues);
+            }
+          },
+          (reason) => {
             reject(reason);
-          });
-          checkReject(i + 1);
-        }
+          }
+        );
       });
-    })(0);
-
-    // 这里检测resolve
-    let resolveValues = [];
-    (function checkResolve(i: number) {
-      if (i < promises.length) {
-        promises[i].then((value) => {
-          resolveValues.push(value);
-          checkResolve(i + 1);
-        }, null);
-      } else {
-        // 如果能走到这里，那么肯定所有promise都resolve了，所以无需再判断resolveValues的长度
-        resolve(resolveValues);
-      }
-    })(0);
+    });
   });
 }
 
